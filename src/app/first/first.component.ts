dfafc404f22532aafbf73582ad223031fe9f37e6
003935 <sorachit.k@cdg.co.th>
2024-02-12 13:29:17 +0700

    add if , for

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,29 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [FormsModule ,InputTextModule],
+  imports: [CommonModule, FormsModule, InputTextModule],
   templateUrl: './first.component.html',
   styleUrl: './first.component.scss'
 })
-export class FirstComponent { 
+export class FirstComponent {
+  colors : string[] = ['Red', 'Blue', 'White'];
+
+  @Input()
+  name?: string;
+  displayName: boolean = true;
+  lastName?: string;
+  displayLastName: boolean = true;
   @Output()
   valueChange = new EventEmitter();
 
-  onKeyup(event : Event) { 
+  onKeyup(event: Event) {
     this.name = (event.target as HTMLInputElement).value;
     this.valueChange.emit(this.name);
   }
 
 
-  @Input()
-  name?: string;
-
-  
   disableInput = false;
 
-  
-  
+
+
+
 }
