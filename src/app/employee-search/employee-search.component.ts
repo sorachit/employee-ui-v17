a37bba98640c1d40ae3f50ad37341da66174f8ba
003935 <sorachit.k@cdg.co.th>
2024-02-15 07:22:24 +0700

    เพิ่ม enum Gender

--- src/app/employee-search/employee-search.component.ts
+++ src/app/employee-search/employee-search.component.ts
@@ -2,30 +2,31 @@ import { Component } from '@angular/core';
 import { Department } from '../model/department';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-
 import { DropdownModule } from 'primeng/dropdown';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { PanelModule } from 'primeng/panel';
+import { Gender } from '../type/gender';
 @Component({
   selector: 'app-employee-search',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,
+  imports: [CommonModule,
+    RouterOutlet,
     PanelModule,
     FormsModule,
     DropdownModule,
     RadioButtonModule,
-    InputTextModule
-    ],
+    InputTextModule,
+  ],
   templateUrl: './employee-search.component.html',
   styleUrl: './employee-search.component.scss'
 })
 export class EmployeeSearchComponent {
+  Gender = Gender;
   firstName?: string;
   lastName?: string;
-  gender?: string;
+  gender?: Gender;
   department?: string;
   departments: Department[] = [
     { "code": 1, "name": "Mavel" },
---
+++ src/app/type/gender.ts
@@ -0,0 +1,3 @@
+export enum Gender {
+    MALE = 'M', FEMALE = 'F'
+}
