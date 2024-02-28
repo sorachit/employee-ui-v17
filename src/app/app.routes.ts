ef7d7569bd2a100347a42ab72b1c56f3447baafc
003935 <sorachit.k@cdg.co.th>
2024-02-28 22:21:00 +0700

    ลบบรรทัดว่าง

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { EmployeeSearchComponent } from './employee-search/employee-search.component';
 import { EmployeeSaveComponent } from './employee-save/employee-save.component';
 import { Mode } from './type/mode';
-
 export const routes: Routes = [
     {'path': '', component: EmployeeSearchComponent },
     {'path': 'save', component: EmployeeSaveComponent, data: { mode: Mode.ADD }},
