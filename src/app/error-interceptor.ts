bd7b005ef5afc85caac807dd63e4c6f6e140eda1
003935 <sorachit.k@cdg.co.th>
2024-03-03 16:20:22 +0700

    delete line

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,11 +1,9 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { catchError, Observable, throwError } from 'rxjs';
-
+import { Observable, catchError, throwError } from 'rxjs';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
     messageService = inject(MessageService)
     intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(httpRequest).pipe(
