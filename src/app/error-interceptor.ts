3b6dd3f60f52a43ee01c79f5521a423f715797b2
003935 <sorachit.k@cdg.co.th>
2024-03-03 15:45:18 +0700

    ลบ log

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -8,7 +8,6 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     messageService = inject(MessageService)
     intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log(httpRequest)
         return next.handle(httpRequest).pipe(
             catchError((error: HttpErrorResponse) => {
                 this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.message });
